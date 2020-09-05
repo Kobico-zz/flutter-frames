9390a1e1bb04f94df30644a5c00392a9ef3223cb
kobi.cohen@sap.com <kobi.cohen@sap.com>
2020-09-05 17:53:53 +0300

    set state on reset

--- lib/main.dart
+++ lib/main.dart
@@ -65,7 +65,17 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void reset() {
-    _counter = 0;
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter = 0;
+
+
+      html.window.parent.postMessage('$_counter', '*');
+    });
   }
 
   @override
